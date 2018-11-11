bb5079687f019c780c848ae692b178dc541376fb
Nikola Velichkov <nikola@velichkov-bg.com>
2018-11-11 10:00:28 +0100

    Updating form post request to not refresh the page, made a functional clock

--- parkalot/static/parkalot/js/script.js
+++ parkalot/static/parkalot/js/script.js
@@ -8,18 +8,51 @@ $(document).ready(function() {
       navigator.geolocation.getCurrentPosition(function(position) {
         submitIfHaveCoordinates(position.coords.latitude, position.coords.longitude);
       });
-    } 
+    }
     else {
       return null
     }
     event.preventDefault();
   });
+
+  setInterval(() => {
+    getCurrentTime($("#current-time"))
+  }, 1000)
 });
+
+function getCurrentTime(element) {
+  let time = new Date()
+  let timeValue = "";
+
+  if (time.getHours() < 10)
+      timeValue += "0"
+  timeValue += time.getHours() + ":"
+  if (time.getMinutes() < 10)
+      timeValue += "0"
+  timeValue += time.getMinutes() + ":"
+  if (time.getSeconds() < 10)
+      timeValue += "0"
+  timeValue += time.getSeconds()
+
+  element.text(timeValue)
+}
 
 // callback to submit form if we got the coordinates
 function submitIfHaveCoordinates(lat,long) {
   if(lat != null && long != null) {
     $('input[name="coordinates"]').val(lat+":"+long);
-    document.getElementById("startParkForm").submit();
+    let formEl = $("#startParkForm")
+
+    $.ajax({
+      url:formEl.attr('action'),
+      type: 'post',
+      data: formEl.serialize(),
+      succsess: () => {
+          console.log("successfully sent data to server")
+      }
+  })
+
+  //get current time and set the second clock for 3 hours later
+    // document.getElementById("startParkForm").submit();
   }
 }
